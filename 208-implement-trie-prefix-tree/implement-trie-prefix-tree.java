2f3ad1c549cc3d1f7f7453d7f506932c21dc6436
Rohit Madabattula <rohitmadabattula2006@gmail.com>
2026-09-07 11:22:14 +0530

    Time: 36 ms (35.41%) | Memory: 61.7 MB (83.73%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,69 @@
+class Trie {
+    class TrieNode{
+        TrieNode trieNode[];
+        boolean WE;
+        TrieNode(){
+            trieNode = new TrieNode[26];
+            WE = false;
+        }
+        Boolean containsChar(char c){
+            return trieNode[c - 'a']!=null;
+        }
+        TrieNode getChar(char c){
+            return trieNode[c - 'a'];
+        }
+        void insert(char c){
+            trieNode[c-'a']=new TrieNode();
+        }
+        void markWordEnd(){
+            WE=true;
+        }
+        Boolean isWord(){
+            return WE;
+        }
+    }
+    TrieNode root;
+    public Trie() {
+        root=new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode temp = root;
+        for(char c: word.toCharArray()){
+            if(!temp.containsChar(c)){
+                temp.insert(c);
+            }
+            temp=temp.getChar(c);
+        }
+        temp.markWordEnd();
+    }
+    
+    public boolean search(String word) {
+        TrieNode temp = root;
+        for(char c: word.toCharArray()){
+            if(!temp.containsChar(c)){
+                return false;
+            }
+            temp=temp.getChar(c);
+        }
+        return temp.isWord();
+    }
+    public boolean startsWith(String prefix) {
+        TrieNode temp = root;
+        for(char c: prefix.toCharArray()){
+            if(!temp.containsChar(c)){
+                return false;
+            }
+            temp=temp.getChar(c);
+        }
+        return temp!=null;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
